e2ba3be7b2bfd058c87e7109b75cdd79ca98e883
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2024-10-23 21:41:54 +0200

    Clean

--- src/utils/standalone_scripts/secrets.py
+++
@@ -1,7 +0,0 @@
-# copernicus
-client_name = 'sh-9e834756-8c66-426e-b753-3f97292e26ae'
-client_secret = '4SFlQyOZBSaNnoAzqVJF2NN3vgGMHUbS'
-
-# aws s3
-aws_access_key = '4HNWZNYG9TIOIFO8OUWR'
-aws_secret_key = 'i94xYWwp3hsgScU12AIkWLTpPMNowh9PVoeIUUxa'
